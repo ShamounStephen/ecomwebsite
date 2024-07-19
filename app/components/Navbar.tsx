9f93dcbe0484ecd6e349512ae2615f6fafd0c26e
ShamounStephen <151960454+ShamounStephen@users.noreply.github.com>
2024-07-19 15:11:43 +0500

    update

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -15,12 +15,12 @@ const links = [
 
 export default function Navbar() {
   const pathname = usePathname();
-  const { handleCartClick } = useShoppingCart();
+  const { handleCartClick, cartCount } = useShoppingCart();
   return (
-    <header className='w-screen overflow-hidden  shadow h-[70px] flex  sticky top-0 z-50 border-b backdrop-filter backdrop-blur-lg bg-opacity-30 bg-white px-4 md:px-0 '>
-      <div className='w-[90%] md:w-[80%] 2xl:max-w-[1280px]  flex justify-between items-center mx-auto '>
+    <header className="w-screen overflow-hidden  shadow h-[70px] flex  sticky top-0 z-50 border-b backdrop-filter backdrop-blur-lg bg-opacity-30 bg-white px-4 md:px-0 ">
+      <div className="w-[90%] md:w-[80%] 2xl:max-w-[1280px]  flex justify-between items-center mx-auto ">
         <Link href="/">
-          <h1 className='text-2xl md:text-3xl font-semibold '>
+          <h1 className="text-2xl md:text-3xl font-semibold ">
             Flip<span className="text-primary">Commerce</span>
           </h1>
         </Link>
@@ -53,9 +53,12 @@ export default function Navbar() {
             className="flex flex-col justify-center items-center h-[60px] w-[70px] md:h-[60px] md:w-[70px] relative md:pt-3"
           >
             <ShoppingBag />
-            <span className="text-xs text-primary hidden md:block">
-              Cart
-            </span>
+            <span className="text-xs text-primary hidden md:block">Cart</span>
+            {cartCount !== undefined && cartCount > 0 && (
+              <div className="w-5 h-5 flex justify-center items-center rounded-full border-primary border-[3px] absolute top-[4px] right-[8px] md:right-[6px] md:top-[3px] text-xs antialiased">
+                {cartCount}
+              </div>
+            )}
           </Button>
         </div>
       </div>
