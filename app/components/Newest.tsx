c738f7600241a61435f4581c649c0b1ddf01a3db
ShamounStephen <151960454+ShamounStephen@users.noreply.github.com>
2024-07-20 07:40:07 +0500

    update

--- app/components/Newest.tsx
+++ app/components/Newest.tsx
@@ -40,6 +40,7 @@ export default async function Newest() {
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {data.map((product) => (
             <div key={product._id} className="group relative">
+              <Link href={`/product/${product.slug}`}>
               <div className="aspect-square w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-80">
                 <Image
                   src={product.imageUrl}
@@ -49,6 +50,7 @@ export default async function Newest() {
                   height={300}
                 />
               </div>
+              </Link>
 
               <div className="mt-4 flex justify-between">
                 <div>
