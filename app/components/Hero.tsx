4b6c23edc83b668d7596f8cedb84d0a5a53ca8ad
ShamounStephen <151960454+ShamounStephen@users.noreply.github.com>
2024-07-23 05:50:49 +0500

    edit new animation

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -16,15 +16,14 @@ export default async function Hero() {
     <section className="mx-auto max-w-2xl px-4 sm:pb-6 lg:max-w-7xl lg:px-8">
       <div className="mb-8 flex flex-wrap justify-between md:mb-16">
         <div className="mb-6 flex w-full flex-col justify-center sm:mb-12 lg:mb-0 lg:w-1/3 lg:pb-24 lg:pt-48">
-          <h1 className="capitalize font-bold text-transparent text-6xl bg-clip-text bg-gradient-to-r from-emerald-600 to-teal-950 dark:from-red-600 dark:to-green-950">
-            Top Fashion for a top price!
-          </h1>
-          <p className="max-w-md leading-relaxed text-gray-500 xl:text-lg">
-            Step into Flip<span className="text-primary">Commerce</span> - Where
-            fashion transcends time. Discover curated collections, exclusive
-            deals, and a shopping experience like no other. Elevate your style
-            quotient with us!
-          </p>
+        <h1 className="capitalize font-bold text-transparent text-6xl bg-clip-text bg-gradient-to-r from-emerald-600 to-teal-950 dark:from-red-600 dark:to-green-950 animate-bubble">
+  Top Fashion for a top price!
+</h1>
+
+          <p className="max-w-md leading-relaxed text-gray-500 xl:text-lg animate-fade-in animate-bounce">
+  Step into Flip<span className="text-primary">Commerce</span> - Where fashion transcends time. Discover curated collections, exclusive deals, and a shopping experience like no other. Elevate your style quotient with us!
+</p>
+
         </div>
         <div className="mb-12 flex w-full md:mb-16 lg:w-2/3">
           <div className="relative left-12 top-12 z-10 -ml-12 overflow-hidden rounded-lg bg-gray-100 shadow-lg md:left-16 md:top-16 lg:ml-0">
